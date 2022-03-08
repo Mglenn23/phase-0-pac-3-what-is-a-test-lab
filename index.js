a0bcbab6526b6d86982daeb4011395e11953b700
michael <michaelglenn88@yahoo.com>
2022-03-08 12:19:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -3,3 +3,92 @@ const height = 24;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+
+
+//console.log(message);
+
+// function doNothing(){
+//     //console.log("Hello!!");
+// }
+
+// function sayHello(){
+//     console.log("Hello!!");
+// }
+// sayHello();
+
+// function sayHelloToIsabel() {
+//     console.log("Hello, Isabel!");
+// }
+// //sayHelloToIsabel();
+
+// function sayHelloToSofia() {
+//     console.log("Hello, Sofia!");
+// }
+// function sayHelloToBrendan() {
+//     console.log("Hello, Brendan!");
+// }
+
+// sayHelloToIsabel();
+// sayHelloToSofia();
+// sayHelloToBrendan();
+
+// function doSomething(thing) {
+//     console.log(thing);
+//   }
+  
+//   doSomething("anything");
+
+// function sayHelloTo(firstName){
+// console.log(`Hello, ${firstName}!`);
+// }
+
+// sayHelloTo("Isabel"); 
+// sayHelloTo("Jane"); 
+// sayHelloTo("R2-D2");
+// sayHelloTo(1);
+
+//console.log(firstName);
+
+// function say(greeting, firstName) {
+//     console.log("firstName: ", firstName);
+//     console.log("greeting: ", greeting);
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+
+//   //say("Goodbye", "Julio");
+//   say("Julio", "hello");
+
+// function add(x, y) {
+//     return x + y;
+// }
+// //console.log(add(1, 2));
+
+// const sum = add(2, 3);
+// const message1 = `The sum of your numbers is: ${sum}.`;
+
+// console.log(message1);
+
+// function say(greeting, firstName) {
+//     return `${greeting}, ${firstName}!`;
+//   }
+
+//   console.log(say("Hello", "Sofia"));
+
+//   function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+
+// function say(greeting, firstName) {
+//     console.log("I was called!");
+//     return `${greeting}, ${firstName}!`;
+    
+//   }
+//   console.log(say("Howdy", "partner"));
+
+
+
+// function countID(x,y){
+//     return x*y;
+// }
+// console.log(countID(4,7));
